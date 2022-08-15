4cd92ba44586eee9438a5099407ab44744060ea8
Fernando Bonfim <devnoob@vivaldi.net>
2022-08-15 08:03:47 -0300

    Configured babel module

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
-function createConfig(customObj){
+function createConfig(customObj) {
   const common = {
     mode: "production",
     devtool: "cheap-module-source-map"
+    module: {
+      rules: [{
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }]
+    }
   }
 
   return {
