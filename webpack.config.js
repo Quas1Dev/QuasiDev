8fe3e923c2c4e486261155c68e86c7b2f0356bd0
Fernando Bonfim <devnoob@vivaldi.net>
2022-08-15 07:40:46 -0300

    Added function to cunstruct custom configs sharing common options

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
-let firstConfig = {
-  mode: 'production',
+function createConfig(customObj){
+  const common = {
+    mode: production,
+    devtool: "cheap-module-eval-source-map"
+  }
+
+  return {
+    ...common,
+    ...customObj
+  }
+}
+
+let firstConfig = createConfig({
   entry: './assets/scripts/sources/posts.js',
   output: {
     filename: "../assets/scripts/posts.min.js"
   }
+})
 
-}
-
-let secondConfig = {
-  mode: 'production',
+let secondConfig = createConfig({
   entry: './assets/scripts/sources/general-script.js',
   output: {
     filename: "../assets/scripts/general-script.min.js"
   }
-
-}
+})
 
 module.exports = [firstConfig, secondConfig]
